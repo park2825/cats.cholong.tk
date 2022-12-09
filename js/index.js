890c5f27b896650b87cdef5b0ac6e04ca9672c07
KlartNET <36400787+JadeMin@users.noreply.github.com>
2022-12-09 22:00:37 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 Array.prototype.getRandom = function(){
 	return this[Math.floor(Math.random() * this.length)];
 };
-const arr = ["https://www.youtube.com/watch?v=JCaP11BfiGw", "https://www.youtube.com/watch?v=JCaP11BfiGw"];
+const randlist = ["https://www.youtube.com/watch?v=JCaP11BfiGw", "https://www.youtube.com/watch?v=JCaP11BfiGw"];
+
 
 jQuery(document).ready(()=> {
-	$("#cataas-img").click(() => location.href = getRandom);
+	$("#cataas-img").click(() => location.href = arr.getRandom());
 });
