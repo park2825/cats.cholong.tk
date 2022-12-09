ac6de3b6e854dc8c2781dc488fd116bdf1df18ff
KlartNET <36400787+JadeMin@users.noreply.github.com>
2022-12-09 21:59:44 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-jQuery(document).ready(()=>{});
+Array.prototype.getRandom = function(){
+	return this[Math.floor(Math.random() * this.length)];
+};
+const arr = ["https://www.youtube.com/watch?v=JCaP11BfiGw", "https://www.youtube.com/watch?v=JCaP11BfiGw"];
+
+jQuery(document).ready(()=> {
+	$("#cataas-img").click(() => location.href = getRandom);
+});
