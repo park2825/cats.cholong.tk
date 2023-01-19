b58f67f19a8940840331b7c21db12e59127cba81
park2825 <62270796+park2825@users.noreply.github.com>
2023-01-20 08:20:34 +0900

    Update keyevent.js

--- js/keyevent.js
+++ js/keyevent.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(document).bind('keydown', 'mousedown',function(e){
+    $(document).bind('keydown',function(e){
         if(e.keyCode == 123) {
             swal({
                 title: '경고!',
@@ -40,18 +40,12 @@ $(document).ready(function(){
             e.returnValue = false;
         }
         
-        if(e.button == 2) {
-            swal({
-                title: '경고!',
-                text: '이미지 저작권 보호를위해 우클릭을 사용할수 없습니다.',
-                icon: 'warning'
-                });
-            e.preventDefault();
-            e.returnValue = false;
-        }
     });
 });
 }
+
+document.onmousedown = 
+    function(){if(button == 2){swal({title: '경고!', text: '이미지 저작권 보호를위해 우클릭을 사용할수 없습니다.', icon: 'warning'}});
 
 document.ondragstart = 
     function(){swal({title: '경고!',text: '이미지 저작권 를 위해 드래그를 사용할수 없습니다.',icon: 'warning'}); return false;}
