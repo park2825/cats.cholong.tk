d520cffb170a926e993e3cefbb643195f105e32b
park2825 <62270796+park2825@users.noreply.github.com>
2023-01-20 04:40:27 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 Array.prototype.getRandom = function(){
 	return this[Math.floor(Math.random() * this.length)];
 };
-const rickrolls = ["https://www.youtube.com/watch?v=0MnfI7IvQ0U", "https://www.youtube.com/watch?v=JCaP11BfiGw"];
+const rickrolls = ["https://www.youtube.com/watch?v=rbz5siyFS9s", "https://www.youtube.com/watch?v=bwQ_jw4lZ3g"];
 
 
 jQuery(document).ready(()=> {
