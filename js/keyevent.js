4e7a36be408d156d9c5d67b635291e95cc77af59
park2825 <62270796+park2825@users.noreply.github.com>
2023-01-20 06:15:14 +0900

    Update keyevent.js

--- js/keyevent.js
+++ js/keyevent.js
@@ -17,4 +17,4 @@ function disableclick(e){
     }
 }
 
-document.onselectstart = function(){alert('저작권 방지를위해 드래그는 사용하실수 없습니다.') return false;}
+document.onselectstart = function(){alert('저작권 방지를위해 드래그는 사용하실수 없습니다.'); return false;}
