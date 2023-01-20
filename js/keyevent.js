de2d9c0d58918f65f27f61beda22c2f657f09eb6
KlartNET <36400787+JadeMin@users.noreply.github.com>
2023-01-20 13:16:41 +0900

    Update keyevent.js

--- js/keyevent.js
+++ js/keyevent.js
@@ -1,46 +1,47 @@
-$(document).ready(function(){
-	$(document).on('keydown', function(e){
-		if(e.keyCode == 123) {
-			swal({
-				title: '경고!',
-				text: '사이트 보호를 위해 개발자도구를 사용하실수 없습니다.',
-				icon: 'warning'
-			});
-			e.preventDefault();
-			return false;
-		}
-
-		if(e.keyCode == 17) {
-			swal({
-				title: '경고!',
-				text: '사이트 보호를 위해 ctrl키를 사용하실수 없습니다.',
-				icon: 'warning'
-			});
-			e.preventDefault();
-			return false;
-		}
-
-		if(e.keyCode == 16) {
-			swal({
-				title: '경고!',
-				text: '사이트 보호를 위해 shift키를 사용하실수 없습니다.',
-				icon: 'warning'
-			});
-			e.preventDefault();
-			return false;
-		}
-
-		if(e.keyCode == 73) {
-			swal({
-				title: '경고!',
-				text: '사이트 보호를 위해 i키를 사용하실수 없습니다.',
-				icon: 'warning'
-			});
-			e.preventDefault();
-			return false;
-		}
+$(document).ready(() => {
+	const $catImage = $("#cataas-img");
+	$(document).on('keydown', event => {
+		switch(event.keyCode) {
+			case 123: {
+				swal({
+					title: '경고!',
+					text: '사이트 보호를 위해 개발자도구를 사용하실수 없습니다.',
+					icon: 'warning'
+				});
+				e.preventDefault();
+				return false;
+			};
+			case 17: {
+				swal({
+					title: '경고!',
+					text: '사이트 보호를 위해 ctrl키를 사용하실수 없습니다.',
+					icon: 'warning'
+				});
+				e.preventDefault();
+				return false;
+			};
+			case 16: {
+				swal({
+					title: '경고!',
+					text: '사이트 보호를 위해 shift키를 사용하실수 없습니다.',
+					icon: 'warning'
+				});
+				e.preventDefault();
+				return false;
+			};
+			case 73: {
+				swal({
+					title: '경고!',
+					text: '사이트 보호를 위해 i키를 사용하실수 없습니다.',
+					icon: 'warning'
+				});
+				e.preventDefault();
+				return false;
+			};
+		};
 	});
-   	$("#cataas-img").on('contextmenu', event => {
+	
+   	$catImage.on('contextmenu', event => {
 		swal({
 			title: '경고!',
 			text: '이미지 저작권 보호를위해 우클릭을 사용할수 없습니다.',
@@ -49,7 +50,7 @@ $(document).ready(function(){
 		event.preventDefault();
 		return false;
 	});
-	$("#cataas-img").on('dragstart', event => {
+	$catImage.on('dragstart', event => {
 		swal({
 			title: '경고!',
 			text: '이미지 저작권 를 위해 드래그를 사용할수 없습니다.',
