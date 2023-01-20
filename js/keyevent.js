08825c4ab36094175c98a9abe38ee5603544beed
KlartNET <36400787+JadeMin@users.noreply.github.com>
2023-01-20 13:12:10 +0900

    Create keyevent.js

--- js/keyevent.js
+++ js/keyevent.js
@@ -1,61 +1,61 @@
 $(document).ready(function(){
-    $(document).on('keydown', function(e){
-        if(e.keyCode == 123) {
-            swal({
-                title: '경고!',
-                text: '사이트 보호를 위해 개발자도구를 사용하실수 없습니다.',
-                icon: 'warning'
-            });
-            e.preventDefault();
+	$(document).on('keydown', function(e){
+		if(e.keyCode == 123) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 개발자도구를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
 			return false;
-        }
-        
-        if(e.keyCode == 17) {
-           swal({
-                title: '경고!',
-                text: '사이트 보호를 위해 ctrl키를 사용하실수 없습니다.',
-                icon: 'warning'
-           });
-            e.preventDefault();
+		}
+
+		if(e.keyCode == 17) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 ctrl키를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
 			return false;
-        }
-        
-        if(e.keyCode == 16) {
-            swal({
-                title: '경고!',
-                text: '사이트 보호를 위해 shift키를 사용하실수 없습니다.',
-                icon: 'warning'
-            });
-            e.preventDefault();
+		}
+
+		if(e.keyCode == 16) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 shift키를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
 			return false;
-        }
-        
-        if(e.keyCode == 73) {
-            swal({
-                title: '경고!',
-                text: '사이트 보호를 위해 i키를 사용하실수 없습니다.',
-                icon: 'warning'
-            });
-            e.preventDefault();
+		}
+
+		if(e.keyCode == 73) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 i키를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
 			return false;
-        }
-    });
-   	$(document.body).on('contextmenu', event => {
-		event.preventDefault();
-        swal({
-                title: '경고!',
-                text: '이미지 저작권 보호를위해 우클릭을 사용할수 없습니다.',
-                icon: 'warning'
-        });
-		return false;
+		}
 	});
-	$(document.body).on('dragstart', event => {
-		event.preventDefault();
+   	$("#cataas-img").on('contextmenu', event => {
 		swal({
 			title: '경고!',
-			text: '이미지 저작권 보호를 위해 드래그를 사용할수 없습니다.',
+			text: '이미지 저작권 보호를위해 우클릭을 사용할수 없습니다.',
 			icon: 'warning'
 		});
+		event.preventDefault();
+		return false;
+	});
+	$("#cataas-img").on('dragstart', event => {
+		swal({
+			title: '경고!',
+			text: '이미지 저작권 를 위해 드래그를 사용할수 없습니다.',
+			icon: 'warning'
+		});
+		event.preventDefault();
 		return false;
 	});
 });
