11c836b5fd805e5caeba5e543dba94a7e2d26dd2
KlartNET <36400787+JadeMin@users.noreply.github.com>
2023-01-20 13:04:52 +0900

    Update keyevent.js

--- js/keyevent.js
+++ js/keyevent.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    $(document).bind('keydown', function(e){
+    $(document).on('keydown', function(e){
         if(e.keyCode == 123) {
             swal({
                 title: '경고!',
                 text: '사이트 보호를 위해 개발자도구를 사용하실수 없습니다.',
                 icon: 'warning'
-                });
+            });
             e.preventDefault();
-            e.returnValue = false;
+			return false;
         }
         
         if(e.keyCode == 17) {
@@ -15,9 +15,9 @@ $(document).ready(function(){
                 title: '경고!',
                 text: '사이트 보호를 위해 ctrl키를 사용하실수 없습니다.',
                 icon: 'warning'
-                });
+           });
             e.preventDefault();
-            e.returnValue = false;
+			return false;
         }
         
         if(e.keyCode == 16) {
@@ -25,9 +25,9 @@ $(document).ready(function(){
                 title: '경고!',
                 text: '사이트 보호를 위해 shift키를 사용하실수 없습니다.',
                 icon: 'warning'
-                });
+            });
             e.preventDefault();
-            e.returnValue = false;
+			return false;
         }
         
         if(e.keyCode == 73) {
@@ -35,28 +35,27 @@ $(document).ready(function(){
                 title: '경고!',
                 text: '사이트 보호를 위해 i키를 사용하실수 없습니다.',
                 icon: 'warning'
-                });
+            });
             e.preventDefault();
-            e.returnValue = false;
+			return false;
         }
     });
-});
-
-
-
-document.onmousedown=disableclick;
-
-function disableclick(e){
-    if(e.button==2) {
+   	$(document).on('contextmenu', event => {
+		event.preventDefault();
         swal({
                 title: '경고!',
                 text: '이미지 저작권 보호를위해 우클릭을 사용할수 없습니다.',
                 icon: 'warning'
-                });
-        e.preventDefault();
-        e.returnValue = false;
-    }
-}
-
-document.ondragstart = 
-    function(){swal({title: '경고!',text: '이미지 저작권 를 위해 드래그를 사용할수 없습니다.',icon: 'warning'}); return false;}
+        });
+		return false;
+	});
+	$(document).on('dragstart', event => {
+		event.preventDefault();
+		swal({
+			title: '경고!',
+			text: '이미지 저작권 를 위해 드래그를 사용할수 없습니다.',
+			icon: 'warning'
+		});
+		return false;
+	});
+});
